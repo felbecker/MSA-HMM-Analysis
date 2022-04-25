745cfc3845e928538c2c23e63327aa4b23f8c781
Ung0d <beckerfelix94@gmail.com>
2022-04-25 12:42:00 +0000

    clustalo run script mkdirs

--- results/clustalo/run.sh
+++ results/clustalo/run.sh
@@ -5,11 +5,14 @@ BALIFAM100="../../data/balifam100/train/*.fasta"
 BALIFAM1000="../../data/balifam1000/train/*.fasta"
 BALIFAM10000="../../data/balifam10000/train/*.fasta"
 
+mkdir -p alignments
+mkdir -p times
+
 run () {
     for f in $1
     do
 	filename=$(basename "$f")
-	{ time clustalo-1.2.4 -t protein -i $f --outfmt=fa -o alignments/$filename ; } 2> times/${filename%.fasta}.time.txt
+	{ time clustalo -t protein -i $f --outfmt=fa -o alignments/$filename ; } 2> times/${filename%.fasta}.time.txt
     done
 }
 
