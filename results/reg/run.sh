229edf577bfb2b037ec1ddb50ace436a7affb6e6
Ung0d <beckerfelix94@gmail.com>
2022-04-29 12:45:22 +0000

    added regressive results except large and run script

---
+++ results/reg/run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+HOMFAM="../../data/homfam/train/*.fasta"
+BALIFAM100="../../data/balifam100/train/*.fasta"
+BALIFAM1000="../../data/balifam1000/train/*.fasta"
+BALIFAM10000="../../data/balifam10000/train/*.fasta"
+
+run () {
+    for f in $1
+    do
+	filename=$(basename "$f")
+	#slow and accurate settings
+	{ time t_coffee -seq $f -reg -nseq 100 -tree mbed -method mafftginsi_msa -outfile "alignments/$filename" -outtree "trees/${filename%.fasta}.mbed" ; } 2> times/${filename%.fasta}.time.txt   
+    done
+}
+
+run "${HOMFAM}"
+run "${BALIFAM100}"
+run "${BALIFAM1000}"
+run "${BALIFAM10000}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
