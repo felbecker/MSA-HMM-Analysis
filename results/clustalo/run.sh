449a4d7677906e9186f83d120066733b5ee797fd
Ung0d <beckerfelix94@gmail.com>
2022-05-03 19:52:50 +0000

    added clustalo balifrag results

--- results/clustalo/run.sh
+++ results/clustalo/run.sh
@@ -4,6 +4,7 @@ HOMFAM="../../data/homfam/train/*.fasta"
 BALIFAM100="../../data/balifam100/train/*.fasta"
 BALIFAM1000="../../data/balifam1000/train/*.fasta"
 BALIFAM10000="../../data/balifam10000/train/*.fasta"
+BALIFRAG="../../data/balifrag/train/*.fasta"
 
 mkdir -p alignments
 mkdir -p times
@@ -11,8 +12,8 @@ mkdir -p times
 run () {
     for f in $1
     do
-	filename=$(basename "$f")
-	{ time clustalo -t protein -i $f --outfmt=fa -o alignments/$filename ; } 2> times/${filename%.fasta}.time.txt
+        filename=$(basename "$f")
+        { time clustalo -t protein -i $f --outfmt=fa -o alignments/$filename ; } 2> times/${filename%.fasta}.time.txt
     done
 }
 
@@ -20,8 +21,7 @@ run "${HOMFAM}"
 run "${BALIFAM100}"
 run "${BALIFAM1000}"
 run "${BALIFAM10000}"
-
-
+run "${BALIFRAG}"
 
 
 
