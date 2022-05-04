d190a999253cb31ce89465e62cd224554658b5a1
Ung0d <beckerfelix94@gmail.com>
2022-05-04 13:46:25 +0000

    updated mafft results and run script, added balifrag results

--- results/mafft/run.sh
+++ results/mafft/run.sh
@@ -4,19 +4,22 @@ HOMFAM="../../data/homfam/train/*.fasta"
 BALIFAM100="../../data/balifam100/train/*.fasta"
 BALIFAM1000="../../data/balifam1000/train/*.fasta"
 BALIFAM10000="../../data/balifam10000/train/*.fasta"
+BALIFRAG="../../data/balifrag/train/*.fasta"
 
 mkdir -p alignments
 mkdir -p times
+
+MAFFT_PATH=""
 
 run () {
     mkdir tmp_files
     for f in $1
     do
 	filename=$(basename "$f")
-	cp $f tmp_tiles/$filename
-	perl replace_U.pl tmp_tiles/$filename
+	cp $f tmp_files/$filename
+	perl replace_U.pl tmp_files/$filename
 	#if using a mafft installation at the default place, drop the -m option
-	{ time mafft-sparsecore.rb -i tmp_tiles/$filename -m/home/felix/bin > alignments/$filename ; } 2> times/${filename%.fasta}.time.txt
+	{ time ${MAFFT_PATH}mafft-sparsecore.rb -i tmp_files/$filename -m ${MAFFT_PATH}/mafft > alignments/$filename ; } 2> times/${filename%.fasta}.time.txt
     done
     rm -r tmp_files
 }
@@ -25,7 +28,7 @@ run "${HOMFAM}"
 run "${BALIFAM100}"
 run "${BALIFAM1000}"
 run "${BALIFAM10000}"
-
+run "${BALIFRAG}"
 
 
 
