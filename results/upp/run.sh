cac165d9c92dba239eb8e12fbbf215580a96ab86
Ung0d <beckerfelix94@gmail.com>
2022-05-04 06:11:32 +0000

    added upp balifrag results

--- results/upp/run.sh
+++ results/upp/run.sh
@@ -4,6 +4,7 @@ HOMFAM="../../data/homfam/train/*.fasta"
 BALIFAM100="../../data/balifam100/train/*.fasta"
 BALIFAM1000="../../data/balifam1000/train/*.fasta"
 BALIFAM10000="../../data/balifam10000/train/*.fasta"
+BALIFRAG="../../data/balifrag/train/*.fasta"
 
 mkdir -p alignments
 mkdir -p times
@@ -11,15 +12,16 @@ mkdir -p times
 run () {
     for f in $1
     do
-	filename=$(basename "$f")
-	mkdir tmp_${filename%.fasta}
-	{ time run_upp.py -M -1 -m amino -s $f -d "tmp_${filename%.fasta}" -o "${filename%.fasta}" ; } 2> times/${filename%.fasta}.time.txt
-	#upp refuses to align if a file with matching prefix exists in the target dir
-	#i.e. if it runs for "blmb" first, it refuses to run on "blm" thereafter
-	#we avoid this by using a tmp dir 
-	mv tmp_${filename%.fasta}/* alignments/
-	mv alignments/${filename%.fasta}_alignment.fasta alignments/$filename
-	rm -r tmp_${filename%.fasta}
+        filename=$(basename "$f")
+        mkdir tmp_${filename%.fasta}
+        { time python /home/bioinf.lan/fbecker/bin_aligners/sepp/run_upp.py -M -1 -m amino -s $f -d "tmp_${filename%.fasta}" -o "$\
+{filename%.fasta}" ; } 2> times/${filename%.fasta}.time.txt
+        #upp refuses to align if a file with matching prefix exists in the target dir
+        #i.e. if it runs for "blmb" first, it refuses to run on "blm" thereafter
+        #we avoid this by using a tmp dir
+        mv tmp_${filename%.fasta}/* alignments/
+        mv alignments/${filename%.fasta}_alignment.fasta alignments/$filename
+        rm -r tmp_${filename%.fasta}
     done
 }
 
@@ -27,17 +29,4 @@ run "${HOMFAM}"
 run "${BALIFAM100}"
 run "${BALIFAM1000}"
 run "${BALIFAM10000}"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+run "${BALIFRAG}"
