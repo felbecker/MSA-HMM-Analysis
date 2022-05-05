f3846f80eefefbb599d8bedb9efb0d695e4fec15
Ung0d <beckerfelix94@gmail.com>
2022-05-05 13:15:08 +0000

    updated MAGUS results and run script

--- results/MAGUS/run.sh
+++ results/MAGUS/run.sh
@@ -1,33 +1,31 @@
-#!/bin/bash
+#!/bin/bash                                                                                                             
 
 HOMFAM="../../data/homfam/train/*.fasta"
 BALIFAM100="../../data/balifam100/train/*.fasta"
 BALIFAM1000="../../data/balifam1000/train/*.fasta"
 BALIFAM10000="../../data/balifam10000/train/*.fasta"
-LARGE="../../data/large/train/*.fasta"
+BALIFRAG="../../data/balifrag/train/*.fasta"
+
+
+mkdir -p alignments
+mkdir -p times
 
 run () {
     for f in $1
     do
-	filename=$(basename "$f")
-	#-t clustal failed for ldh, idh and rvp, used -fasttree instead
-	if [ ! -f "alignments/$filename" ]; then
-	    if [ $filename == "ldh.fasta" ] || [ $filename == "idh.fasta" ] || [ $filename == "rvp.fasta" ]; then
-		{ time python3 /home/felix/MAGUS/magus.py -d ./work_${filename%.fasta} -t fasttree -i $f -o "alignments/$filename" ; } 2> times/${filename%.fasta}.time.txt
-	    else
-		{ time python3 /home/felix/MAGUS/magus.py -d ./work_${filename%.fasta} -t clustal -i $f -o "alignments/$filename" ; } 2> times/${filename%.fasta}.time.txt
-	    fi
-	fi
+        filename=$(basename "$f")
+        if [ ! -f "alignments/$filename" ]; then
+            { time python3 /home/bioinf.lan/fbecker/bin_aligners/MAGUS/magus.py -d ./work_${filename%.fasta} --recurse \
+false -t clustal -i $f -o "alignments/$filename" ; } 2> times/${filename%.fasta}.time.txt
+        fi
     done
 }
 
-run "${HOMFAM}"
-run "${BALIFAM100}"
+run "${HOMFAM}"                                                                                                        
+run "${BALIFAM100}"                                                                                                    
 run "${BALIFAM1000}"
-run "${BALIFAM10000}"
-run "${LARGE}"
-
-
+run "${BALIFAM10000}"                                                                                                  
+run "${BALIFRAG}"                                                                                                      
 
 
 
