d33002117e81c6e95921c84b6bf4410b93d174ef
Ung0d <beckerfelix94@gmail.com>
2022-08-05 07:11:44 +0000

    learnMSA new results

---
+++ results/msa_hmm2/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+HOMFAM="../../data/homfam/train/*.fasta"
+BALIFAM100="../../data/balifam100/train/*.fasta"
+#BALIFAM1000="../../data/balifam1000/train/*.fasta"
+BALIFAM10000="../../data/balifam10000/train/*.fasta"
+BALIFRAG="../../data/balifrag/train/*.fasta"
+LARGE="../../data/large/train/*.fasta"
+
+OUT_DIR="./"
+
+mkdir -p ${OUT_DIR}alignments
+mkdir -p ${OUT_DIR}logs
+mkdir -p ${OUT_DIR}times
+
+run () {
+    for f in $1
+    do
+	filename=$(basename "$f")
+	if [ ! -f "${OUT_DIR}alignments/$filename" ]; then
+        { time python3 ../../../MSA-HMM/MsaHmm.py -i "$f" -o "${OUT_DIR}alignments/$filename" > "${OUT_DIR}logs/${filename%.fasta}.log" ; } 2> "${OUT_DIR}times/${filename%.fasta}.time.txt"
+    fi
+    done
+}
+
+run "${HOMFAM}"
+run "${BALIFAM100}"
+#run "${BALIFAM1000}"
+run "${BALIFAM10000}"
+run "${BALIFRAG}"
+run "${LARGE}"
